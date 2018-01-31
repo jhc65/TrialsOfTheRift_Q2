de7f45ab8d2695fe21df311eec6f487ba5fc618f
dtinpa <dtdthomp54@gmail.com>
2018-01-31 15:24:32 -0500

    added fix to unstuck a puck from the portal.  Also optimized the pucks by reducing the number of invokes.

--- TrialsOfTheRiftWC/Assets/Scripts/HockeyPuckController.cs
+++ TrialsOfTheRiftWC/Assets/Scripts/HockeyPuckController.cs
@@ -11,6 +11,8 @@ public class HockeyPuckController : MonoBehaviour {
     public bool b_scored = false;   // identifies when the puck has been used to score
     private Vector3 v3_home;        // location of flag in players' base
     private Rigidbody rb;
+    private bool isDecreaseInvoked = false;
+    private bool isPuckStuck = false;
 
     void Start()
     {
@@ -22,17 +24,33 @@ public class HockeyPuckController : MonoBehaviour {
 
     private void Update()
     {
-        //if the Puck hasn't been hit by a spell or parry, start the speed cooldown
-        InvokeRepeating("DecreaseSpeed", Constants.ObjectiveStats.C_SpeedDecayDelay, Constants.ObjectiveStats.C_SpeedDecayRate);
+
+        Vector3 v3_rightPortal = new Vector3(37.25f, 0.5f, -15.25f);
+        Vector3 v3_leftPortal = new Vector3(-37.25f, 0.5f, 15.25f);
+
+        //calls the function if the puck is believed to be stuck.  This way of invoking saves frames
+        if (!isPuckStuck && (transform.position == v3_rightPortal || transform.position == v3_leftPortal)) {
+            Invoke("PuckIsStuckInPortal", 5.0f);
+            Debug.Log("We're stuck in Portal potentially.");
+            isPuckStuck = true;
+        }
 
         if (transform.position.x > 0)
             e_Side = Constants.Global.Side.RIGHT;
         else
             e_Side = Constants.Global.Side.LEFT;
 
-        if (f_speed < Constants.ObjectiveStats.C_PuckBaseSpeed) {
-            CancelInvoke();
+        if (f_speed < Constants.ObjectiveStats.C_PuckBaseSpeed)
+        {
+            CancelInvoke("DecreaseSpeed");
+            isDecreaseInvoked = false;
             f_speed = Constants.ObjectiveStats.C_PuckBaseSpeed;
+        }
+        //used to make sure only 1 invoke of this function is made, and not every frame
+        else if (!isDecreaseInvoked){
+            //if the Puck hasn't been hit by a spell or parry, start the speed cooldown
+            InvokeRepeating("DecreaseSpeed", Constants.ObjectiveStats.C_SpeedDecayDelay, Constants.ObjectiveStats.C_SpeedDecayRate);
+            isDecreaseInvoked = true;
         }
 
         Vector3 v3_dir = rb.velocity.normalized;
@@ -68,6 +86,7 @@ public class HockeyPuckController : MonoBehaviour {
         {
             //we need to get the direction the player is facing, so that's why v3_direction is verbose
             CancelInvoke();
+            isDecreaseInvoked = false;
             f_speed += Constants.ObjectiveStats.C_HitIncreaseSpeed;
             Vector3 v3_direction = other.gameObject.transform.parent.gameObject.transform.forward.normalized;
             transform.rotation = Quaternion.LookRotation(other.gameObject.transform.parent.gameObject.transform.forward);
@@ -84,7 +103,8 @@ public class HockeyPuckController : MonoBehaviour {
             collision.gameObject.GetComponent<PlayerController>().TakeDamage(Constants.ObjectiveStats.C_PuckDamage);
         }
         if (collision.gameObject.tag == "Spell") {
-            CancelInvoke();
+            CancelInvoke("DecreaseSpeed");
+            isDecreaseInvoked = false;
         }
         if (collision.gameObject.tag == "Floor")
         {
@@ -124,5 +144,20 @@ public class HockeyPuckController : MonoBehaviour {
     //used to decrement the speed every second after 3 seconds
     private void DecreaseSpeed() {
         f_speed -= Constants.ObjectiveStats.C_SpeedDecreaseRate;
+    }
+
+    //if the puck gets stuck in the portal, move it over from it and reset its speed
+    private void PuckIsStuckInPortal() {
+
+        Vector3 v3_rightPortal = new Vector3(37.25f, 0.5f, -15.25f);
+        Vector3 v3_leftPortal = new Vector3(-37.25f, 0.5f, 15.25f);
+        if (transform.position == v3_rightPortal) {
+            transform.position = new Vector3(32.25f, 0.5f, -15.25f);
+        }
+        if (transform.position == v3_leftPortal) {
+            transform.position = new Vector3(-32.25f, 0.5f, 15.25f);
+        }
+
+        isPuckStuck = false;
     }
 }
