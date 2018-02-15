13051820463ed3b6e55b9f4c89213305baef302e
Joe <jhc65@drexel.edu>
2018-02-15 01:20:37 -0500

    - destroy bolt on hit - remove `player-player` from matrix

--- TrialsOfTheRiftWC/Assets/Scripts/RiftDeathbolt.cs
+++ TrialsOfTheRiftWC/Assets/Scripts/RiftDeathbolt.cs
@@ -6,7 +6,6 @@ public class RiftDeathbolt : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        Debug.Log("calling DestroyDeathbolt()");
         Invoke("DestroyDeathbolt", Constants.RiftStats.C_VolatilityDeathboltLiveTimer);
 	}
 	
@@ -16,19 +15,13 @@ public class RiftDeathbolt : MonoBehaviour {
 	}
 
     private void OnCollisionEnter(Collision collision) {
-        Debug.Log("Collision: " + collision.gameObject.name);
-        if (collision.gameObject.tag != "Player") {
-            //ignores any collision that isn't with a player
-            Physics.IgnoreCollision(GetComponent<Collider>(), collision.gameObject.GetComponent<Collider>());
-        }
-        else {
-            // DIE! DIE! DIE!
-            collision.gameObject.GetComponent<PlayerController>().TakeDamage(Constants.RiftStats.C_VolatilityDeathboltDamage);
-        }
+        // DIE! DIE! DIE!
+        collision.gameObject.GetComponent<PlayerController>().TakeDamage(Constants.RiftStats.C_VolatilityDeathboltDamage);
+        DestroyDeathbolt();
+
     }
 
     private void DestroyDeathbolt() {
-        Debug.Log("Destroying the deathbolt!");
         Destroy(gameObject);
     }
 }
