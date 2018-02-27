8e7c43b597bb56601aaeb16d4c5941f735c680c6
dtinpa <dtdthomp54@gmail.com>
2018-02-27 17:18:20 -0500

    clean

--- TrialsOfTheRiftWC/Assets/Scripts/RoomInitializer.cs
+++ TrialsOfTheRiftWC/Assets/Scripts/RoomInitializer.cs
@@ -13,22 +13,18 @@ public class RoomInitializer : MonoBehaviour
 
     [SerializeField] private GameObject[] go_spawnerPool;         // rooms allowed for this objective to be played in
     [SerializeField] private Constants.Global.Side side;
-    private RiftController rc_riftController;
+    private RiftController riftController;
 
     void Awake() {
-        rc_riftController = RiftController.Instance;
+        riftController = RiftController.Instance;
     }
 
     void OnEnable() {
         if (side == Constants.Global.Side.RIGHT) {
-            rc_riftController.RightEnemySpawners = go_spawnerPool;
+            riftController.RightEnemySpawners = go_spawnerPool;
         }
         else {
-            rc_riftController.LeftEnemySpawners = go_spawnerPool;
+            riftController.LeftEnemySpawners = go_spawnerPool;
         }
-    }
-
-    private void Start() {
-    
     }
 }
