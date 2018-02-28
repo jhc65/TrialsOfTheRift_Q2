7f2c6859e8c54c12435396727920021ae08f6bf4
SlightPlus <nero.DDX@gmail.com>
2018-02-27 21:19:45 -0500

    > Added enemyIndiCanvas.prefab and enemyAlert.png (Assets/Art/UI/enemyAlert.png). > Added functionality and some hooks in Rift object in main scene. > Added Destroy clause in CameraFacingBillboard for non player objects. > Readjusted prefabs to fix certain issues.

--- TrialsOfTheRiftWC/Assets/Scripts/CameraFacingBillboard.cs
+++ TrialsOfTheRiftWC/Assets/Scripts/CameraFacingBillboard.cs
@@ -12,14 +12,20 @@ using UnityEngine;
 public class CameraFacingBillboard : MonoBehaviour {
 
 	public Camera cam_Camera;
-    public PlayerController pc_trackedPlayer;
+    public GameObject go_trackedObject;
     public Vector3 v3_offset;
 
     /*/////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
 
+    private void Start() {
+        if (go_trackedObject.GetComponent<PlayerController>() == null) {
+            Destroy(gameObject, 1.0f);
+        }
+    }
+
     void Update() {
         transform.LookAt(transform.position + cam_Camera.transform.rotation * Vector3.forward,
             cam_Camera.transform.rotation * Vector3.up);
-        transform.position = pc_trackedPlayer.transform.position + v3_offset;
+        transform.position = go_trackedObject.transform.position + v3_offset;
     }
 }
